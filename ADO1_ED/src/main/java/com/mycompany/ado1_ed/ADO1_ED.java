fa596efb0006099f9a178a17fdedabdea9d79163
giovanna ramos <161260067+idlrwheel@users.noreply.github.com>
2024-09-09 21:55:08 -0300

    Add files via upload

---
+++ ADO1_ED/src/main/java/com/mycompany/ado1_ed/ADO1_ED.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ado1_ed;
+
+/**
+ *
+ * @author Giovanna
+ */
+public class ADO1_ED {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ ADO1_ED/src/main/java/com/mycompany/ado1_ed/Cliente.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ado1_ed;
+
+public class Cliente {
+    private String nome;
+    private int id;
+    private int idade;
+    
+}
