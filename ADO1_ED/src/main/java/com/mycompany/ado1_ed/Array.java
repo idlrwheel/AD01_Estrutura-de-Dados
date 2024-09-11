0836b09e5fd3a226fd05b4a87b94f8d5a7d04898
giovanna ramos <161260067+idlrwheel@users.noreply.github.com>
2024-09-11 20:01:24 -0300

    Create Array.java
    Criação do vetor clientes

---
+++ ADO1_ED/src/main/java/com/mycompany/ado1_ed/Array.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ado1_ed;
+
+public class Array{
+  Cliente [ ] clientes = new Cliente [10];
+  
+}
