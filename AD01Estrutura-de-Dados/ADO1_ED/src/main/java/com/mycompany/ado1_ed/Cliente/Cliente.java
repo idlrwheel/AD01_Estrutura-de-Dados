fac7b1bcd9ef893049306bbb88bb28b994d5e39f
Vitorwgs <167483937+Vitorwgs@users.noreply.github.com>
2024-09-13 21:53:10 -0300

    Add files via upload

---
+++ AD01Estrutura-de-Dados/ADO1_ED/src/main/java/com/mycompany/ado1_ed/Cliente/Cliente.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ado1_ed;
+
+public class Cliente {
+    
+    private String nome;
+    private int id;
+    private int idade;
+    private String telefone;
+    private String email;
+    private static int valor=1;
+
+    public Cliente (){
+    }
+
+    public Cliente (String nome, int id, int idade, String telefone, String email){
+        this.nome = nome;
+        this.idade = idade;
+        this.telefone = telefone;
+        this.email = email;
+        id=valor++;
+    }
+
+    public String getNome(String nome){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public int getId(){
+        return id;
+    }
+    
+    public int getIdade(){
+        return idade;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+    public String getTelefone(){
+        return telefone;
+    }
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    @Override
+    public String toString(){
+    return "Cliente {" + 
+                     "nome= " + nome + 
+                     ", idade= " + idade +
+                     ", telefone= " + telefone + 
+                     ", email= " + email + '}';
+    }
+    
+}
+
---
+++ AD01Estrutura-de-Dados/ADO1_ED/src/main/java/com/mycompany/ado1_ed/Vetor/Array.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ado1_ed;
+
+public class Array{
+
+  private static Cliente[] clientes = new Cliente[12];
+  private static int  indice = 0;
+
+  public static void cadastrar(Cliente cliente) {
+    if(indice < clientes.length){
+      clientes=alocarclientesArray();
+    }
+    clientes[indice++] = cliente;
+  }
+
+  public static void exibir(){
+    for(int i=0; i<indice; i++){
+      System.out.println(clientes[i]);
+    }
+  }
+
+  public static int pesquisar(int id){
+    for (int i=0; i<indice; i++){
+      if(clientes[i].getId() == id){
+        return i;
+      }
+    }
+    return -1;
+  }
+  public static Cliente pesquisarCadastro(int id){
+    for (int i = 0; i < indice; i++)
+    if(i == clientes[i].getId()){
+      return clientes[i];
+    }
+    }
+    return null;
+}
+
+public static bolean excluir(int id){
+  int indRem = pesquisar(id);
+  if ( indRem >= 0){
+    for(int i = indRem; i<indice-1; i++){
+      clientes[i] = clientes[i+1];
+    }
+    indice--;
+    return true;
+  }
+  retrun false;
+}
+
+public static Cliente[] alocarclientesArray(){
+  Cliente[] novo=new Cliente[clientes.length+2];
+
+  for (int i = 0; i < clientes.length; i++) {
+      novo[i] = clientes[i];
+  }
+  retur novo;
+}
+  
